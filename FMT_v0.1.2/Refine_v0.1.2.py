1335e9e4915cce493ed26f9cc76fa26e257b6440
unknown <umakhan97@mail.ru>
2024-09-17 03:08:51 +0300

    Omitted

--- FMT_v0.1.2/Refine_v0.1.2.py
+++ FMT_v0.1.2/Refine_v0.1.2.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
 import os
 import configparser
+import math
 
 def calculate_cs(xmin, xmax, imin):
     return (xmax - xmin) / imin
